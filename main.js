4d5c2adafb13d74992e914b1335f8aa1bff29845
Karen Kanana <kananakaren21@gmail.com>
2022-08-31 13:39:27 +0300

    Complete assignment

--- main.js
+++ main.js
@@ -5,6 +5,36 @@ const FULL_HEART = '♥'
 // Your JavaScript code goes here!
 
 
+const articleHearts = document.querySelectorAll(".like-glyph");
+
+for (const glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback)
+}
+
+modal.classList.add("hidden")
+
+function likeCallback(e) {
+  const heart = e.target;
+  mimicServerCall("bogusUrl")
+    .then(function(){
+      if ( heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART;
+        heart.className = "activated-heart";
+      } else {
+        heart.innerText = EMPTY_HEART;
+        heart.className = "";
+      }
+    })
+
+    .catch(()=>{
+      modal.classList.remove("hidden")
+      const modal = document.getElementById("modal");
+      modal.classList = "";
+      modal.querySelector('h2').innerText = error;
+      setTimeout(() => {modal.classList.add("hidden")}, 3000);
+    })
+}
+
 
 
 //------------------------------------------------------------------------------
